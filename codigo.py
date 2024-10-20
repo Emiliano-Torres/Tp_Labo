032915a1146ccd6608be1f68af89e6c59fbaf25c
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-10-20 18:24:42 -0300

    opcion 2 tercer grafico

--- codigo.py
+++ codigo.py
@@ -612,3 +612,27 @@ ax.set_xticks(np.linspace(1,48,num=48))
 ax.set_xticklabels(df_grafico['nombre_pais'],rotation=90)
 plt.grid(True, linestyle='--', color='gray', linewidth=0.7)
 plt.tight_layout()
+#%%
+flujo_migratorio_hacia_argentina=sql^"""SELECT origen, cantidad FROM flujos_migratorios 
+                                        WHERE destino='ARG' AND origen!='ARG' AND año=2000 AND cantidad !=0"""
+#tomamos la decision de ignorar los registros con 0 flujo migratorio
+chequeo=sql^"""SELECT origen, cantidad FROM flujos_migratorios 
+                                        WHERE destino='ARG' AND origen!='ARG' AND año=2000 AND cantidad =0"""
+#ya que solo tienen una sede con 0 migracion
+df_chequeo=sql^"""SELECT origen, cantidad, cantidad_de_sedes FROM sedes_por_codigo_pais
+                  INNER JOIN chequeo ON origen=pais_iso_3"""
+
+df_grafico=sql^"""SELECT nombre_pais, cantidad, cantidad_de_sedes FROM sedes_por_codigo_pais
+                  INNER JOIN flujo_migratorio_hacia_argentina ON origen=pais_iso_3
+                  INNER JOIN pais AS p ON p.pais_iso_3=origen """
+
+fig,ax = plt.subplots(figsize=(10,8))
+ax.scatter(x=df_grafico['cantidad'].apply(int),y=np.linspace(1,1000,num=len(df_grafico['nombre_pais'])),s=5*df_grafico['cantidad_de_sedes'].apply(int), label='Flujo migratorio por paises')
+size_legend = ax.scatter([], [],color='blue' ,s=100, alpha=0.7, label='Cantidad de Sedes')
+ 
+ax.set_yticks(np.linspace(1,1000,num=len(df_grafico['nombre_pais'])))
+ax.set_yticklabels(df_grafico['nombre_pais'])
+plt.grid(True, linestyle='--', color='gray', linewidth=0.7)
+ax.legend()
+# Ajustar el espacio vertical entre las etiquetas
+plt.tight_layout()
